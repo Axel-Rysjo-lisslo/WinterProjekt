94dfc1a1d5fb730c2c371eb008e71dff97215000
AxelRysjöLisslö <rysjolisslo.axel@elev.ga.ntig.se>
2022-12-01 10:21:24 +0100

    cmoinmit

---
+++ WinterProjekt/Player.cs
@@ -0,0 +1,22 @@
+using System;
+
+
+public class Player
+{
+    public Vector2 spawn { get; set; } = new(800, 500);
+    public int speed { get; set; } = 3;
+    
+
+
+    public int heigth{ get; set; } = 60;
+    public int width  { get; set; } = 60;
+
+}
+
+
+if (R_IsKeyDown(KeyboardKey.KEY_D))
+{
+
+
+    
+}
--- WinterProjekt/Program.cs
+++ WinterProjekt/Program.cs
@@ -1,1 +1,26 @@
-﻿using Raylib_cs;
+﻿global using System;
+global using System.Numerics;
+global using Raylib_cs;
+global using R = Raylib_cs.Raylib;
+
+Raylib.InitWindow(1600,800,"spelFönster");
+Raylib.SetTargetFPS(60);
+
+Player p = new Player();
+
+while (!R.WindowShouldClose()) {
+
+    R.BeginDrawing();
+    R.ClearBackground(Color.WHITE);
+
+
+    R.EndDrawing();
+  
+
+
+}
+
+
+
+    
+Console.ReadLine();
