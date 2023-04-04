4db14fc5bb64c381fdb3500956d1aeb7559ea8ee
AxelRysjöLisslö <rysjolisslo.axel@elev.ga.ntig.se>
2023-04-04 12:53:07 +0200

    komentar

--- WinterProjekt/GlobalProp.cs
+++ WinterProjekt/GlobalProp.cs
@@ -2,6 +2,7 @@ using System;
 
 
 // variabel för att kolla hur många meteor som spawnar
+// variabel för basespeed på meteorn 
 public static class GlobalProp
 {
     public static int TotalMeteorSpawned { get; set; } = 0;
